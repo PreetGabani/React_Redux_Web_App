0f94f943239e673f5f84841d9c8c184119520db3
PreetGabani <preetgabani123@gmail.com>
2022-01-21 17:49:30 +0530

    21/1/2022 5:49PM

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "../src/store"
+import store, { persistor } from "../src/store"
 import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react"
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store.js
+++ src/store.js
@@ -1,6 +1,13 @@
 import RootReducer from "../src/Redux/reducer/reducerindex"
 import { createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+const persitConfig={
+    key:'persist-store',
+    storage
+}
+const PersistReducer = persistReducer(persitConfig,RootReducer)
+const store = createStore(PersistReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const persistor = persistStore(store)
 export default store;
